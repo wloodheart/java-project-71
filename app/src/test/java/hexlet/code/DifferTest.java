1127b2337d52fb8066501fefb914368401c4655d
wlood heart <wloodheart@gmail.com>
2023-12-03 20:27:36 +0300

    edited tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -21,30 +21,9 @@ public class DifferTest {
                   + verbose: true
                 }""";
 
-        String data1 = """
-                {
-                  "host": "hexlet.io",
-                  "timeout": 50,
-                  "proxy": "123.234.53.22",
-                  "follow": false
-                }""";
-
-        String data2 = """
-                {
-                  "timeout": 20,
-                  "verbose": true,
-                  "host": "hexlet.io"
-                }""";
-
         Path filePath1 = Path.of("src/test/resources/file1.json");
         Path filePath2 = Path.of("src/test/resources/file2.json");
 
-        Files.writeString(filePath1, data1);
-        Files.writeString(filePath2, data2);
-
         assertThat(Differ.generate(filePath1.toFile(), filePath2.toFile())).isEqualTo(expected);
-
-        Files.delete(filePath1);
-        Files.delete(filePath2);
     }
 }
